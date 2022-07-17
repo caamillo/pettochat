c893512b95481ee67117be5543fc6102bf57d451
caamillo <diaskygotah@gmail.com>
2022-07-18 00:31:34 +0200

    tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,36 @@
+// React
 import { useState, useEffect, useRef } from 'react'
 
+// Tailwind
+import './tailwind/tailwind.css'
+import './tailwind/output.css'
+
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+
+    const [uid, setUid] = useState()
+    const [roomId, setRoomId] = useState()
+
+    const myVid = useRef()
+    const usrsVid = []
+
+    useEffect(() => {
+        // get UID and ROOMID
+        if (localStorage.getItem('roomId') != null && localStorage.getItem('uid') != null) {
+            setUid(localStorage.getItem('uid'));
+            setRoomId(localStorage.getItem('roomId'))
+        } else {
+            // Fetch from server
+        }
+
+
+    }, [])
+
+    return (
+        <div className='h-full flex items-center justify-center'>
+            <div id="video-grid" className='grid grid-cols-2 gap-4'>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  darkMode: 'class',
+  content: [ "./src/*.js", "./public/*.html" ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

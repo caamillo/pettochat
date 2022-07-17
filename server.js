375ce00a59dd7f78808a0a40d052f4cb617db93b
camillo <diaskygotah@gmail.com>
2022-07-17 20:17:52 +0200

    no manually create rooms

--- server.js
+++ server.js
@@ -4,15 +4,20 @@ const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
 
+const links = []
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.redirect(`/${uuidV4()}`)
+    const link = uuidV4()
+    links.push(link)
+    res.redirect(`/${link}`)
 })
 
 app.get('/:room', (req, res) => {
-    res.render('room', { roomId: req.params.room })
+    if (links.includes(req.params.room) || req.params.room == 'favicon.ico') res.render('room', { roomId: req.params.room })
+    else res.redirect('/')
 })
 
 io.on('connection', socket => {
@@ -25,4 +30,6 @@ io.on('connection', socket => {
     })
 })
 
-server.listen(3000)
+server.listen(3000, '127.0.0.1', () => {
+    console.log('Server started at: http://127.0.0.1:3000')
+})

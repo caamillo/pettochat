cb7f40d855910baefe2a03689041ef30ac4e8edb
caamillo <diaskygotah@gmail.com>
2022-07-18 02:12:36 +0200

    fixing cors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,62 @@ import { useState, useEffect, useRef } from 'react'
 import './tailwind/tailwind.css'
 import './tailwind/output.css'
 
+// Socket
+import io from 'socket.io-client'
+
+// Peerjs
+import { Peer } from "peerjs";
+
+const socket = io.connect('http://localhost:5000', {transports: ['websocket', 'polling', 'flashsocket']})
+
 function App() {
 
     const [uid, setUid] = useState()
     const [roomId, setRoomId] = useState()
 
     const myVid = useRef()
+    const [myStream, setMyStream] = useState()
+
     const usrsVid = []
+
+    const socket = io('/')
+    const myPeer = new Peer(undefined, {
+        host: '/',
+        port: '5001'
+    })
+
+    const peers = {}
+    const [videos, setVideos] = useState([])
+
+    const addVideoStream = (video, stream) => {
+        video.srcObject = stream
+        video.addEventListener('loadedmetadata', () => {
+            video.play()
+        })
+        setVideos([...videos, video])
+        console.log(videos)
+    }
+
+    // Init vids
+    useEffect(() => {
+        const myVideoSource = document.createElement('video')
+        myVideoSource.muted = true
+        navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio: true
+        }).then(stream => {
+            setMyStream(stream)
+            addVideoStream(myVideoSource, stream)
+
+            myPeer.on('call', call => {
+                call.answer(stream)
+                const video = document.createElement('video')
+                call.on('stream', userVideoStream => {
+                    addVideoStream(video, userVideoStream)
+                })
+            })
+        })
+    }, [])
 
     useEffect(() => {
         // get UID and ROOMID
@@ -19,15 +68,36 @@ function App() {
             setUid(localStorage.getItem('uid'));
             setRoomId(localStorage.getItem('roomId'))
         } else {
-            // Fetch from server
+            console.log(socket)
+            socket.on('get-room', roomId => {
+                setRoomId(roomId)
+            })
+            myPeer.on('open', id => {
+                setUid(id)
+                socket.emit('join-room', roomId, id)
+            })
         }
 
+        if (uid == null || roomId == null) return console.log('Error Room / UID', uid, roomId)
+        console.log(uid, roomId)
 
     }, [])
+
+    useEffect(() => {
+        const videosDisplayed = document.getElementsByTagName('video')
+        let c = 0
+        for (const vid of videosDisplayed) {
+            console.log('update videos')
+            console.log(videos[c].srcObject)
+            vid.srcObject = videos[c].srcObject
+            c += 1
+        }
+    }, [videos])
 
     return (
         <div className='h-full flex items-center justify-center'>
             <div id="video-grid" className='grid grid-cols-2 gap-4'>
+                { videos.map(v => <video key={ v.srcObject.id }></video>) }
             </div>
         </div>
     );
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
 const server = require('http').Server(app)
-const io = require('socket.io')(server)
+const io = require('socket.io')(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: [ 'GET', 'POST' ]
+    }
+})
 const { v4: uuidV4 } = require('uuid')
 
 const links = []
@@ -9,18 +14,15 @@ const links = []
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    const link = uuidV4()
-    links.push(link)
-    res.json({ link: link })
-})
-
 app.get('/:room', (req, res) => {
     if (links.includes(req.params.room) || req.params.room == 'favicon.ico') res.json({ roomId: req.params.room })
     else res.status(404).json({ roomId: null })
 })
 
 io.on('connection', socket => {
+    const link = uuidV4()
+    links.push(link)
+    socket.emit('get-room', link)
     socket.on('join-room', (roomId, userId) => {
         socket.join(roomId)
         socket.to(roomId).emit('user-connected', userId)
